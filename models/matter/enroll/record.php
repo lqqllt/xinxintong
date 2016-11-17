1dc88af301f6f197f8616ec3fb7b9404228129e4
lt <lqqllt@163.com>
2016-11-17 09:11:38 +0800

    Revert "修改了搜索添加能搜索特殊字符"
    This reverts commit 73d88a5f1558845f9567cfb9b30e5d37a866fbc5.
    
    Signed-off-by: lt <lqqllt@163.com>

--- models/matter/enroll/record.php
+++ models/matter/enroll/record.php
@@ -230,21 +230,18 @@ class record_model extends \TMS_MODEL {
 		$whereByData = '';
 		foreach ($data as $k => $v) {
 			if (!empty($v) && is_string($v)) {
-				
-					if(preg_match("/\\\\/", $v)){
-						$v=preg_replace("/\\\\/", "\\\\\\", $v);
-					}
 
-					$v=$this->escape($v);					
-					$s=htmlspecialchars($v,ENT_QUOTES);
+				$v=preg_replace("/\\\\/", "\\\\\\", $v);				
+				$v=$this->escape($v);					
+				$s=htmlspecialchars($v,ENT_QUOTES);
 								
-					$whereByData .= ' and (';
-					$whereByData .= 'data like \'%"' . $k . '":"' . $v . '"%\'';
-					$whereByData .= ' or data like \'%"' . $k . '":"%,' . $v . '"%\'';
-					$whereByData .= ' or data like \'%"' . $k . '":"%,' . $v . ',%"%\'';
-					$whereByData .= ' or data like \'%"' . $k . '":"' . $v . ',%"%\'';
-					$whereByData .= ' or data like \'{"' . $k . '":"%' . $s . '%"}\'';
-					$whereByData .= ')';
+				$whereByData .= ' and (';
+				$whereByData .= 'data like \'%"' . $k . '":"' . $v . '"%\'';
+				$whereByData .= ' or data like \'%"' . $k . '":"%,' . $v . '"%\'';
+				$whereByData .= ' or data like \'%"' . $k . '":"%,' . $v . ',%"%\'';
+				$whereByData .= ' or data like \'%"' . $k . '":"' . $v . ',%"%\'';
+				$whereByData .= ' or data like "%'.$s.'%"';
+				$whereByData .= ')';
 			}
 		}
 
@@ -386,11 +383,8 @@ class record_model extends \TMS_MODEL {
 			$whereByData = '';
 			foreach ($criteria->data as $k => $v) {
 				if (!empty($v)) {
-
-					if(preg_match("/\\\\/", $v)){
-						$v=preg_replace("/\\\\/", "\\\\\\", $v);
-					}
-
+					
+					$v=preg_replace("/\\\\/", "\\\\\\", $v);				
 					$v=$this->escape($v);					
 					$s=htmlspecialchars($v,ENT_QUOTES);
 								
@@ -399,7 +393,7 @@ class record_model extends \TMS_MODEL {
 					$whereByData .= ' or data like \'%"' . $k . '":"%,' . $v . '"%\'';
 					$whereByData .= ' or data like \'%"' . $k . '":"%,' . $v . ',%"%\'';
 					$whereByData .= ' or data like \'%"' . $k . '":"' . $v . ',%"%\'';
-					$whereByData .= ' or data like \'{"' . $k . '":"%' . $s . '%"}\'';
+					$whereByData .= ' or data like "%'.$s.'%"';
 					$whereByData .= ')';
 				}
 			}
